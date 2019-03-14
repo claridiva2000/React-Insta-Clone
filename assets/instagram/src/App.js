fdc9ced6f644ae9aeddbbbf18a0d202ac45f7b33
claridiva2000 <claridiva2000@gmail.com>
2019-03-13 20:45:46 -0500

    initial commit2

--- assets/instagram/src/App.js
+++ assets/instagram/src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
+import SearchBar from "./components/SearchBar/SearchBar"
 
-import SearchBar from "./components/SearchBar";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <SearchBar />
+    <SearchBar />
       </div>
     );
   }
